dcdf8fb9f9d43ceebe0f2e728d32eca50821aade
Barath K <127032804+barath-sk17@users.noreply.github.com>
2024-03-12 19:14:03 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -32,18 +32,48 @@ def calculate_returns(price_data):
 
 def calculate_state(returns):
     states = []
+    a = []
     for ret in returns:
         if ret >= 0.01:
             states.append("Bull")
+            a.append(1)
         elif ret > -0.01:
             states.append("Flat")
         else:
             states.append("Bear")
+    print(len(a))
     return states
 
+def portfolio_calculator(states):
+    new_val = [0]*len(states)
+    for i in range(len(states)-1):
+        if states[i] == "Flat" and states[i+1] == "Bull":
+            new_val[i+1] = new_val[i]+1
+        elif states[i] == "Flat" and states[i+1] == "Bear":
+            new_val[i+1] = new_val[i]-1
+        else:
+            new_val[i+1] = new_val[i]
+    return new_val[-1]
+
+def calculate_optimal_indices(states,returns):
+    store_indices = []
+    for i in range(len(states)-1):
+        if states[i] == "Bear" and states[i+1] == "Bull":
+            store_indices.append(i+1)
+        elif states[i] == "Flat" and states[i+1] == "Bull":
+            store_indices.append(i+1)
+        
+        # might consider in certain cases
+        
+        # elif (states[i] == "Bull" and states[i+1] == "Bull") and (returns[i]<returns[i+1]):
+        #    store_indices.append(i+1)
+        
+
+    return store_indices
+
 def calculate_transition_distribution(states):
-    state_transitions = {'Flat': {'Bull': 0, 'Flat': 0, 'Bear': 0}, 'Bull': {'Bull': 0, 'Flat': 0, 'Bear': 0}, 'Bear': {'Bull': 0, 'Flat': 0, 'Bear': 0}}
-    prev_state = 'Flat'
+    state_transitions = {'Bear': {'Bear': 0, 'Flat': 0, 'Bull': 0},'Flat': {'Bear': 0, 'Flat': 0, 'Bull': 0}, 'Bull': {'Bear': 0, 'Flat': 0, 'Bull': 0}}
+    prev_state = 'Bear'
     
     for state in states:
         state_transitions[prev_state][state] += 1
@@ -57,29 +87,20 @@ def calculate_transition_distribution(states):
 def main(price_data):
     returns = calculate_returns(price_data)
     states = calculate_state(returns)
+    portfolio_val = portfolio_calculator(states)
+    indices = calculate_optimal_indices(states,returns)
     transition_distribution = calculate_transition_distribution(states)
-    
-    V = 0  # Initial portfolio value
-    optimal_buy_indices = []  # Store indices where buy orders are placed
-    
-    for i, state in enumerate(states):
-        # Update portfolio value based on rules
-        if state == 'Bull' and (i == 0 or states[i - 1] == 'Flat'):
-            V += 1
-            optimal_buy_indices.append(i)
-        elif state == 'Bear' and (i == 0 or states[i - 1] == 'Flat'):
-            V -= 1
-            optimal_buy_indices.append(i)
-    
     # Display results using Streamlit
     st.title('Portfolio Analysis')
     st.subheader('Portfolio Value (V(N)):')
-    st.markdown(f"<div class='value'>{V}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='value'>{portfolio_val}</div>", unsafe_allow_html=True)
     st.markdown(f"")
     st.subheader('Optimal Buy Indices:')
     with st.expander("Optimal Buy Indices"):
-        df = pd.DataFrame.from_dict(optimal_buy_indices)
+        df = pd.DataFrame.from_dict(indices)
         st.dataframe(df)
+    st.markdown(f"")
+    st.markdown(f"<div class='value'>Length : {len(indices)}</div>", unsafe_allow_html=True)
     st.markdown(f"")
     st.subheader('Transition Distribution:')
     # Display transition distribution in a collapsible section
@@ -87,9 +108,12 @@ def main(price_data):
         df = pd.DataFrame.from_dict(transition_distribution)
         st.dataframe(df)
 
+
 # Read dataset
 data = pd.read_csv("QuantRocket.csv")  # Replace "your_dataset.csv" with the path to your dataset
 price_data = data[data['Field'] == 'Close']['FIBBG000B9XRY4'].tolist()
+
+
 
 # Run the Streamlit app
 if __name__ == '__main__':
